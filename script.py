6cfb52cd1ec294aebce64aa0aa6709d7d5e5bd1f
ashwinkanchana <ashwinkanchana@gmail.com>
2021-08-20 12:31:14 +0530

    added string formatting

--- script.py
+++ script.py
@@ -115,8 +115,7 @@ studentData = [['Murielle Nockles', '2021-06-06'],
                ['Kristoforo Gerrans', '2021-01-10'],
                ['Julius Renfree', '2021-04-22'],
                ['Pippo Prendeguest', '2020-10-03'],
-               ['Horatio Peat', '2021-05-04'],
-               ]
+               ['Horatio Peat', '2021-05-04']]
 
 insertData(studentData)
 
